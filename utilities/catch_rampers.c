fdd10082843514c1781feacc912454ca9c62d0e8
mce <mce@35c7d0d9-0540-0410-a198-bddc676e2512>
2008-08-11 23:33:52 +0000

    gain masks and updated catch_rampers program. - MFH

--- utilities/catch_rampers.c
+++ utilities/catch_rampers.c
@@ -20,6 +20,9 @@ int main(int argc, char **argv)
 
 	double* data_max = malloc(frame_size * sizeof(double));
 	double* data_min = malloc(frame_size * sizeof(double));
+	int* flips = malloc(frame_size * sizeof(int));
+	__u32 *last_state = malloc(frame_size * sizeof(__u32));
+	__u32 flip_mask = 0x40000000;
 
 	FILE *fin = fopen(argv[1], "r");
 	if (fin==NULL) {
@@ -35,8 +38,9 @@ int main(int argc, char **argv)
 	
 	__u32 data[4096];
 	
-	__u32 mode_mask = 0x00ffffff;
-	__u32 mode_sign = 0x00800000;
+	__u32 mode_mask = 0xffffff00;
+	__u32 mode_sign = 0x80000000;
+	double rescale = 1./256.;
 
 	int first = 1;
 	int offset = 43;
@@ -51,31 +55,46 @@ int main(int argc, char **argv)
 		if (first) {
 			for (i=0; i<count; i++) {
 				__u32 d = (data[i+offset]) & mode_mask;
-				if (d & mode_sign) d |= 0xff000000;
-				double x = (*((int*)&d)) /gain;
+/* 				if (d & mode_sign) d |= 0xff000000; */
+				double x = (*((int*)&d)) /gain * rescale;
 //				printf("%i %u %lf\n", i, d, x);
 				
 				data_max[i] = x;
 				data_min[i] = x;
+				last_state[i] = data[i+offset] & flip_mask;
 			}
 			first = 0;
 		} else {
 			for (i=0; i<count; i++) {
+				int me = (i == 4*32 + 4);
 				__u32 d = (data[i+offset]) & mode_mask;
-				if (d & mode_sign) d |= 0xff000000;
+/* 				if (d & mode_sign) d |= 0xff000000; */
 /* 				double x = (int)d; */
-				double x = (*((int*)&d)) / gain;
+				double x = (*((int*)&d)) / gain * rescale;
 
+/* 				if (me) printf("%ui\n", d); */
 				if (x > data_max[i]) data_max[i] = x;
 				if (x < data_min[i]) data_min[i] = x;
+				if ((d & flip_mask) != last_state[i]) {
+					flips[i]++;
+					last_state[i] = d & flip_mask;
+				}
+				
+
 			}
 		}		
 	}
 
 	for (i=0; i<count; i++) {
-		int x = (((data_max[i] - data_min[i])) > 8096./2);
-		if (x || 1) {
+		int x = (((data_max[i] - data_min[i])) > 8000.);
+		if (x) {
 			printf("Ramper? %i %i = r%02ic%02i   %lf to %lf\n",
+			       x,
+			       i, i / 32, i % 32, data_min[i], data_max[i]);
+		}
+		x = flips[i];
+		if (x > 1000) {
+			printf("Flipper? %i %i = r%02ic%02i   %lf to %lf\n",
 			       x,
 			       i, i / 32, i % 32, data_min[i], data_max[i]);
 		}
