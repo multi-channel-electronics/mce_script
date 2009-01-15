da5d5b67f655172d0da585e9ffcc75992cf7e11f
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2009-01-15 21:32:05 +0000

    mce_data.py (transitional) can handle arbitrary column count. Interface has changed somewhat.

--- python/test.py
+++ python/test.py
@@ -1,16 +1,18 @@
-import pylab
 from mce_data import *
 from mce_runfile import *
 
-filename='/home/mhasse/Desktop/MBAC-May-Coldrun/fast_1211064182'
-runfile=filename+'.run'
-rf = MCERunfile(runfile)
+#filename='/home/mhasse/Desktop/MBAC-May-Coldrun/fast_1211064182'
+filename='/data/cryo/current_data/z07'
+rf = MCERunfile(filename+'.run')
 print rf.Item('HEADER','RB rc2 num_rows', type='int')
 print rf.Item2d('HEADER', 'RB bc%i bias', type='int', first=1)
 
+filename='/data/cryo/current_data/test_0811'
+d = SmallMCEFile(filename=filename, runfile=False)
+b = d.Read(force_dict=True, row_col=True, data_mode=1)
+print len(b.data['fb'][0][1])
+b = d.Read(force_dict=False, data_mode=2)
+print len(b.data[2])
 
-d = SmallMCEFile(filename=filename, do_read = False)
-b = d.Read(col=0, row=0, force_dictionary = True, field_list = ['all'])
-print b.data.keys()
-pylab.plot(b.data['error'])
-pylab.show()
+dd = d.Read(raw_frames=True)
+print dd[:,1]
