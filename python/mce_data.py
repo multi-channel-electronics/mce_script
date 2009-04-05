fd26b86939f437c2aa7a2155ab07c91ba9977212
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2009-04-05 21:48:16 +0000

    Removed 'transitional' warnings, and improved the n_frames logic in Read.

--- python/mce_data.py
+++ python/mce_data.py
@@ -1,16 +1,15 @@
 import numpy
 from mce_runfile import *
+from os import stat
 
-def digits(n, psych=2):
-    s = ''
-    m = n
-    for i in range(32):
-        s = '%i%s' % (m & 1, s)
-        m = m / 2
-    return s
+# This is what an MCE looks like.
+MCE_RC = 4
+MCE_COL = 8
+MCE_DWORD = 4
 
 # This maximum is to keep memory usage reasonable.
-MAX_FRAMES = 50000
+MAX_READ_SIZE = int(1e8)
+
 
 class HeaderFormat:
     """
@@ -36,6 +35,9 @@ class HeaderFormat:
         self.footer_size = 1
 
 class BitField(object):
+    """
+    Describes the truncation and packing of a signal into a carrier word.
+    """
     def define(self, name, start, count, scale=1.):
         self.name = name
         self.start = start
@@ -57,7 +59,12 @@ class BitField(object):
             data = data * self.scale
         return data
 
+
 class DataMode(dict):
+    """
+    A DataMode consists of a set of BitFields describing signal
+    packing into MCE data words.
+    """
     def __init__(self):
         dict.__init__(self)
         self.fields = []
@@ -67,11 +74,14 @@ class DataMode(dict):
             self[a.name] = a
         return self
 
+
 #Define the MCE data modes
+
 MCE_data_modes = { \
     '0': DataMode().define(BitField().define('error', 0, 32)),
     '1': DataMode().define(BitField().define('fb', 0, 32, 2.**-12)),
     '2': DataMode().define(BitField().define('fb_filt', 0, 32)),
+    '3': DataMode().define(BitField().define('raw', 0, 32)),
     '4': DataMode().define(BitField().define('fb', 14, 18),
                            BitField().define('error', 0, 14)),
     '9': DataMode().define(BitField().define('fb_filt', 8, 24, 2.**1),
@@ -79,6 +89,7 @@ MCE_data_modes = { \
     '10': DataMode().define(BitField().define('fb_filt', 7, 25, 2.**3),
                             BitField().define('fj', 0, 7)),
 }
+
 
 class MCEData:
     """
@@ -89,8 +100,6 @@ class MCEData:
         self.source = None
         self.start_frame = 0
         self.n_frames = 0
-        self.row = None
-        self.col = None
         self.header = None
         self.data_is_dict = False
         self.data = []
@@ -104,7 +113,7 @@ class SmallMCEFile:
     flat-files.  Extraction and rescaling of data content is performed
     automatically by default.
     """
-    def __init__(self, filename=None, runfile=True):
+    def __init__(self, filename=None, runfile=True, basic_info=True):
         """
         filename:   path to MCE flatfile
         runfile:    if True (default), filename.run is used.  If False, no runfile is used.
@@ -122,29 +131,40 @@ class SmallMCEFile:
         if self.runfile == True:
             self.runfile = filename+'.run'
         self.runfile_data = None
+        if filename != None and basic_info:
+            self.ReadHeader()
+            self.ReadRunfile()
+            self.n_frames, _ = self.FrameCount()
 
-    def ReadHeader(self):
+    def FrameCount(self):
+        file_words = stat(self.filename).st_size / MCE_DWORD
+        return file_words / self.frame_size, file_words % self.frame_size
+
+    def ReadHeader(self, offset=None):
         fin = open(self.filename)
+        if offset != None:
+            fin.seek(offset)      
         # It's a V6, or maybe a V7.
         format = HeaderFormat()
         head_binary = numpy.fromfile(file=fin, dtype=numpy.uint32, \
-                                         count=format.header_size)
-
+                                     count=format.header_size)
         # Lookup each offset and store
         self.header = {}
         for k in format.offsets:
             self.header[k] = head_binary[format.offsets[k]]
+        # Decode some entries to determine the frame size
         self.header['rc_present'] = [(self.header['status'] & (1 << 10+i))!=0 \
-                                         for i in range(4)]
+                                         for i in range(MCE_RC)]
         self.header_size = format.header_size
         self.footer_size = format.footer_size
         self.n_rows = self.header['num_rows_reported']
-        # On rectangle firmware mode, n_cols is in bits 16:19 of the status word
+        # On rectangle mode firmware, n_cols is in bits 16:19 of the status word
         if self.header['header_version'] <= 6:
-            self.cols_per_card = 8
+            self.cols_per_card = MCE_COL
         else:
             self.cols_per_card = (self.header['status'] >> 16) & 0xf
         self.n_cols = self.cols_per_card*self.header['rc_present'].count(True)
+        self.frame_size = self.n_rows*self.n_cols + self.header_size + self.footer_size
 
     def ReadRunfile(self):
         """
@@ -156,46 +176,66 @@ class SmallMCEFile:
         self.runfile_data = MCERunfile(self.runfile)
         return self.runfile_data
 
-    def ReadRaw(self, dets=None, n_frames=MAX_FRAMES, start=0, raw_frames=False):
+    def ReadRaw(self, count=None, start=0, dets=None, raw_frames=False):
+        """
+        Load raw data from MCE flatfile.  Reads a frame header to
+        determine basic file structure.  Most users will prefer the
+        Read() method.
+        """
+        # We can determine the frame size once we read a single frame header
         if self.header == None:
             self.ReadHeader()
-
-        # TRANSITIONAL... check runfile for num_cols!
-        print 'TRANSITIONAL, FIX ME'
+        # Rectangle mode transitional - confirm that runfile agrees with frame status
         rf = self.ReadRunfile()
         if rf != None:
             cols = rf.Item('HEADER', 'RB sys num_cols_reported', type='int')
-            if cols != None:
-                if cols[0] != self.cols_per_card:
-                    print 'Correcting frame size using runfile num_cols_reported...'
-                    self.cols_per_card = cols[0]
-                    self.n_cols = self.cols_per_card*self.header['rc_present'].count(True)
+            if cols != None and cols[0] != self.cols_per_card:
+                print 'Bug: runfile cols_reported disagrees with frame header.'
+                self.cols_per_card = cols[0]
+                self.n_cols = self.cols_per_card*self.header['rc_present'].count(True)
+                self.frame_size=self.n_rows*self.n_cols + self.header_size + self.footer_size
+        
+        n_frames, left_over = self.FrameCount()
+        if (left_over) != 0:
+            print 'File %s contains partial frame.'%self.filename
+        # Do the count logic, warn user if something is amiss
+        if start > n_frames:
+            print 'Requested frames at %i, beyond end of file!'%(start)
+            start = n_frames
+        if count == None:
+            count = n_frames - start
+        if count < 0:
+            count = n_frames - start + count
+        if n_frames < start + count:
+            print 'Only %i of requested %i frames can be read.' % (n_frames-start, count)
+            count = n_frames - start
+        if count * self.frame_size * MCE_DWORD > MAX_READ_SIZE:
+            print 'Warning: attempting read of more than %i bytes!'%MAX_READ_SIZE
 
-        size=self.n_rows*self.n_cols + self.header_size + self.footer_size
-
+        # Open, seek, read.
         fin = open(self.filename)
-        fin.seek(4*start*size)
-
-        count = size*n_frames
-        a = numpy.fromfile(file=fin, dtype=numpy.uint32, count=count)
-        self.n_frames = len(a) / size
-        shape = (self.n_frames, size)
-        a = a.reshape(shape)
-
+        fin.seek(start*self.frame_size*MCE_DWORD)
+        a = numpy.fromfile(file=fin, dtype=numpy.uint32, count=count*self.frame_size)
+        n_frames = len(a) / self.frame_size
+        if len(a) != count*self.frame_size:
+            print 'Read problem: %i items requested, %i items returned.'% \
+                  (count*self.frame_size, len(a))
+        a.shape = (n_frames, self.frame_size)
+        # Refresh header information
+        self.ReadHeader(offset=start*self.frame_size*MCE_DWORD)
+        # Return data
         if raw_frames:
             return numpy.cast['int32'](a)
         if dets != None:
             det_offsets = [self.header_size + r * self.n_cols + c for (r, c) in dets]
             return numpy.cast['int32'](a[:,det_offsets]).transpose()
-        return numpy.cast['int32'](a[:,self.header_size:self.header_size+self.n_rows*self.n_cols].transpose())
+        return numpy.cast['int32'](a[:, \
+            self.header_size:self.header_size+self.n_rows*self.n_cols].transpose())
 
     def NameChannels(self, row_col=False):
         if self.header == None:
             self.ReadHeader()
         rc_p = self.header['rc_present']
-
-        if row_col:
-            print 'FIXME: row_list and col_list are still long when row_col==True'
 
         # We need the runfile to properly designate the readout rows/cols
         if self.runfile_data == None:
@@ -204,35 +244,38 @@ class SmallMCEFile:
                 self.row_list = [ i/self.n_cols for i in range(self.n_cols * self.n_rows)]
                 self.col_list = [ i%self.n_cols for i in range(self.n_cols * self.n_rows)]
                 return
-
-        row_index = [ self.runfile_data.Item('HEADER', 'RB rc%i readout_row_index' %(i+1), type='int', array = False) \
-                                       for i in range(4) if rc_p[i] ]
-        col_index = [ self.runfile_data.Item('HEADER', 'RB rc%i readout_col_index' %(i+1), type='int', array = False) \
-                                       for i in range(4) if rc_p[i] ]
+        rfi = self.runfile_data.Item
+        row_index = [ rfi('HEADER', 'RB rc%i readout_row_index' %(i+1),
+                          type='int', array=False) for i in range(MCE_RC) if rc_p[i] ]
+        col_index = [ rfi('HEADER', 'RB rc%i readout_col_index' %(i+1),
+                          type='int', array=False) for i in range(MCE_RC) if rc_p[i] ]
         self.row_list = []
         self.col_list = []
         for r in range(self.n_rows):
             for ri in row_index:
-                self.row_list.extend([r + ri] * 8)
+                self.row_list.extend([r + ri] * MCE_COL)
         # Not all firmware supports readout_col_index...
         if col_index[0] == None:
             # Columns are entirely determined by what readout cards are present
-            rc_cols = [i for i in range(32) if rc_p[i/8]]
+            rc_cols = [i for i in range(MCE_COL*MCE_RC) if rc_p[i/MCE_COL]]
         else:
             # Adjust for RC-dependent readout index
-            rc_cols = [ 8*rc+col_index[rc]+i for i in range(8) for rc in range(4) if rc_p[rc] ]
+            rc_cols = [ MCE_COL*rc+col_index[rc]+i for i in range(MCE_COL)
+                        for rc in range(MCE_RC) if rc_p[rc] ]
         self.col_list = rc_cols * self.n_rows
 
-    def Read(self, dets=None, n_frames=MAX_FRAMES, start=0,
+    def Read(self, count=None, start=0, dets=None,
              do_extract=True, do_scale=True, data_mode=None,
              field=None, fields=None, row_col=False,
-             raw_frames=False):
+             raw_frames=False,
+             n_frames=None):
         """
         Read MCE data, and optionally extract the MCE signals.
 
         dets        Pass a list of (row,col) tuples of detectors to extract (None=All)
-        n_frames    Number of frames to read
-        start       Index of first frame to read
+        count       Number of frames to read (default=None, which means all of them).
+                    Negative numbers are referred to the end of the file.
+        start       Index of first frame to read (default=0).
         do_extract  if True, extract signal bit-fields using data_mode from runfile
         do_scale    if True, rescale the extracted bit-fields to match a reference
                     data mode.
@@ -248,22 +291,24 @@ class SmallMCEFile:
         raw_frames  if True, return a 2d array containing raw data (including header
                     and checksum), with indices (frame, index_in_frame).
         """
-
+        if n_frames != None:
+            print 'Read: Use of n_frames is deprecated, please use the "count=" argument.'
+            count = n_frames
         self.ReadRunfile()
         if raw_frames:
-            data = self.ReadRaw(n_frames=n_frames, start=start, raw_frames=True)
+            data = self.ReadRaw(count=count, start=start, raw_frames=True)
             return data
 
-        data = self.ReadRaw(dets=dets, n_frames=n_frames, start=start)
+        data = self.ReadRaw(dets=dets, count=count, start=start)
         self.NameChannels(row_col=row_col)
         
         data_out = MCEData()
         data_out.source = self.filename
-        data_out.n_frames = n_frames
+        data_out.n_frames = data.shape[1]
         data_out.header = self.header
         data_out.row_list = self.row_list
         data_out.col_list = self.col_list
-
+        # Determine data_mode
         if data_mode == None:
             if self.runfile == False:
                 print 'No runfile, forcing data mode 0'
@@ -275,42 +320,46 @@ class SmallMCEFile:
                     return None
                 rf = self.runfile_data
                 acq_rc = rf.Item('FRAMEACQ', 'RC', type='int')
-                data_mode = rf.Item('HEADER', 'RB rc%i data_mode' % \
-                                    acq_rc[0], type='int', array=False)
+                modes = [rf.Item('HEADER', 'RB rc%i data_mode' % \
+                                 rc, type='int', array=False) for rc in acq_rc]
+                data_mode = modes[0]
+                if modes.count(data_mode) != len(modes):
+                    print 'Mixed data_mode acquisition! Using data_mode %i from rc%i\n'% \
+                          (data_mode, acq_rc[0])
 
-        # NEW CODE
+        # Extract data from carrier words
         dm_data = MCE_data_modes.get('%i'%data_mode)
         if dm_data == None:
             print 'Unimplemented data mode %i, treating as 0.'%data_mode
             dm_data = MCE_data_modes['0']
 
-        # This singlular plural thing is a bit lame...
+        # User passes field=... for single field, or fields=[...] for multi
         if field == None:
             field = 'default'
-
         force_dict = (fields != None)
         if fields == None:
             fields = [field]
         elif fields == 'all':
             fields = dm_data.fields
-
         for i,f in enumerate(fields):
             if f=='default':
                 fields[i] = dm_data.fields[0]                
-
         data_out.data_is_dict = (len(fields) > 1 or force_dict)
         if data_out.data_is_dict:
             data_out.data = {}
 
+        # Extract each field
         for f in fields:
             # Use BitField.extract to get each field
             new_data = dm_data[f].extract(data, do_scale=do_scale)
             if row_col:
-                new_data.shape = (self.n_rows, self.n_cols, self.n_frames)
+                new_data.shape = (self.n_rows, self.n_cols, data_out.n_frames)
             if data_out.data_is_dict:
                 data_out.data[f] = new_data
             else:
                 data_out.data = new_data
-
         return data_out
+
+# Let's just hope that your MCEFile is a Small One.
+MCEFile = SmallMCEFile
 
