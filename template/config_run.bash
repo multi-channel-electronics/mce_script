2f93b5719473ff6e15d6fc2f2ed418c895fcf145
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2010-01-22 01:51:24 +0000

    Associate experiment.cfg and config scripts with tuning analyses so that runfile is kept up to date when old tunings are reused.

--- template/config_run.bash
+++ template/config_run.bash
@@ -15,4 +15,18 @@ fi
 
 mce_cmd -qf $mce_script
 
+# Change the last_squid_tune link
+LAST_TUNE=/data/cryo/last_squid_tune
+SOURCE_TUNE=/data/cryo/${tune_id}
+
+# ... but only if everything seems reasonable.
+if [ -e "$SOURCE_TUNE" ] && ![ -d "$SOURCE_TUNE" ] && ( \
+    ( [ -h "$LAST_TUNE" ] && rm "$LAST_TUNE" ) || \
+    ! [ -e "$LAST_TUNE" ] ); then
+    ln -s "$SOURCE_TUNE" "$LAST_TUNE"
+else
+    echo "Problem linking $SOURCE_TUNE to $LAST_TUNE" >& 2
+    exit 1
+endif
+
 #END config_run.bash
