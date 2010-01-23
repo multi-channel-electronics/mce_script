d43f599caf75216ff3176150137fd92b6bf1a675
dvw <dvw@35c7d0d9-0540-0410-a198-bddc676e2512>
2010-01-23 01:08:23 +0000

    Remove debugging code

--- python/auto_setup/series_array.py
+++ python/auto_setup/series_array.py
@@ -131,7 +131,6 @@ def reduce(tuning, ramp_data, slope=None):
     # Analyze the 'best' SA curves for lock-points
     scale = max([8 * n_fb / 400, 1])
     y = servo.smooth(array([d[i] for d,i in zip(av_vol, bias_idx)]), scale)
-    print av_vol.shape, y.shape
     x_offset = scale/2
     dy = y[:,1:] - y[:,:-1]
     y = y[:,:-1]
@@ -186,148 +185,6 @@ def reduce(tuning, ramp_data, slope=None):
         result[k+'_x'] = fb[result[k+'_idx']]
     return result
 
-def pasdfasdfsdf():
-
-    if ramp_bias:
-        # Make an n_bias + 1 pages set of plots.
-
-        plot_file = directory + "analysis/" + file_name + "_ssa.ps"
-
-        for m in range(n_bias):
-            sa_bias = numpy.zeros(8) + bias_start + m * bias_step
-            page_label = "%s = %i" % (vert_label, sa_bias)
-            for j in range(8):
-                # Plot error bars if keyword set.  Error bars are value +- stdev
-                if (errors):
-                    numpy.errorbar(i_fb, av_vol[m, ..., j],
-                        yerr=dev_vol[i, ...])
-                else:
-                    numpy.plot(i_fb, av_vol[m, ..., j])
-                numpy.xlabel(horiz_label + i_units)
-                # IDL's ytitle
-                numpy.figtext(0,0.5,"Output Voltage" + v_units,
-                    rotation='vertical', ha='center', va='center')
-                numpy.suptitle("RC%i SA Ch %i   peak-to-peak=%g" % (card, j,
-                    estim_range[m,j]))
-
-            numpy.figtext(0, 1, full_name) # Print filename as title
-            numpy.figtext(0.6, 1, page_label) # Print SA_BIAS on title line
-
-        # Merit function calculated from the peak-to-peak values
-        for chan in range(8):
-            numpy.plot((bias_start + arange(n_bias) * bias_step) / 1000.,
-                    estim_range[...,chan])
-            numpy.xtitle('sa_bias (/1000)')
-            numpy.ytitle('peak-to-peak')
-            numpy.suptitle('Series Array Channel %i   Card %i' % (chan, card))
-
-        numpy.figtext(0, 1, full_name)
-        numpy.figtext(0.6, 1, 'Merit function: peak-to-peak')
-
-    for chan in range(8):
-        ind[chan] = estim_range[...,chan].argmax() # method: peak-to-peak
-
-    if (not quiet):
-        print """
-###########################################################################
-SA bias and target (adc_offset) channel by channel:
-###########################################################################
-  Channel Bias@step (index) Target@half  sa_fb@half 
----------------------------------------------------
-        """
-
-    for chan in range(8):
-        # MFH - This effective scale is inherited from the original 400 code.
-        scale = 5 * n_fb / 400.;
-        if scale < 1:
-            scale = 1
-
-        deriv_av_vol = idl_compat.deriv(i_fb,
-                idl_compat.smooth(av_vol[ind[chan], ..., chan], scale))
-        final_sa_bias_ch_by_ch[chan] = round(bias_start + ind[chan] * bias_step)
-
-        # Find position of an SA minimum.  Search range depends on desired
-        # locking slope because we will eventually need to find an SA max.
-        if (s > 0):
-            min_start = scale * 4
-            min_stop = n_fb * 5 / 8
-        else:
-            min_start = n_fb * 3 / 8
-            min_stop = n_fb - scale * 4
-
-#        ind_min = [ind[chan],min_start:min_stop,chan].argmin() + min_start
-
-        # Now track to the side, waiting for the slope to change.
-        if (slope > 0):
-            start = ind_min + scale * 2
-            stop = n_fb
-            step = 1
-        else:
-            start = ind_min - 2 * scale
-            stop = -1
-            step = -1
-          
-        idx = start + step * numpy.arange((stop - start) * step)
-        slope_change = numpy.argwhere(deriv_av_vol[idx] * slope < 0)
-
-        if (slope_change.size == 1):
-            ind_max = stop - step
-        else:
-            ind_max = idx[slope_change.min()]
-
-        # Lock on half-way point between minimum and maximum
-        ind_half_point = round(0.5 * (ind_min + ind_max))
-        target_half_point_ch_by_ch[chan] = round(1000. * av_vol[ind[chan],
-            ind_half_point,chan])
-        fb_half_point_ch_by_ch[chan] = round(1000. * i_fb[ind_half_point])
-
-        if (not quiet):
-            print "%4i %11i %8i %12i %12i" % (chan,
-                final_sa_bias_ch_by_ch[chan], ind[chan],
-                target_half_point_ch_by_ch[chan],
-                fb_half_point_ch_by_ch[chan])
-
-    SA_target = target_half_point_ch_by_ch
-    SA_fb_init = fb_half_point_ch_by_ch
-
-    if (ramp_bias):
-        for chan in range(8):
-            if ((final_sa_bias_ch_by_ch[chan] > 65535) or
-                    (final_sa_bias_ch_by_ch[chan] <= 0)):
-                if (not quiet):
-                    print """
-###########################################################################
-
-WARNING: SA bias of channel """, chan, """ has been set to zero bacause
-                  the program found a non valid value
-
-###########################################################################
-                    """
-                else:
-                    print "%s: SA bias of channel %i is invalid (%i), setting to 0" % \
-                            (__name__, chan, final_sa_bias_ch_by_ch[chan])
-                final_sa_bias_ch_by_ch[chan] = 0
-                ind[chan] = 0
-            if ((SA_fb_init[chan] > 65536) or (SA_fb_init[chan] <= 0)):
-                if (not quiet):
-                    print """
-###########################################################################
-
-WARNING: SA fb of channel """, chan, """ found on the SA V-phi curve has
-                  been set to 32000 bacause the program found a non valid value
-
-###########################################################################
-                    """
-                else:
-                    print "%s: SA fb of channel %i is invalid (%i), setting to 0" % \
-                            (__name__, chan, SA_fb_init[chan])
-                SA_fb_init[chan] = 32000
-
-    return {"final_sa_bias_ch_by_ch": final_sa_bias_ch_by_ch,
-            "SA_target": SA_target,
-            "SA_fb_init": SA_fb_init}
-
-
 def load_ramp_data(filename, reduce=False):
     m = MCEFile(filename)
     rf, data = m.runfile, m.Read(row_col=True).data
@@ -374,8 +231,6 @@ def plot(tuning, ramp_data, lock_points, plot_file=None, format='pdf'):
         data = array([d[i] for d,i in zip(data, lock_points['sa_bias_idx'])])
 
     # Plot plot plot
-    print "fb=",fb.shape
-    print "data=",data.shape
     servo.plot(fb, data.reshape(-1, len(fb)), lock_points, plot_file,
                title=ramp_data['basename'],
                titles=['Column %i - SA_bias=%6i' %(c,b) \
