293f7bf4bc3227ae00bf41ebaa74c27b88527b65
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2010-01-25 21:16:35 +0000

    When tune_id is not set, config_run.bash will link last_squid_tune to /dev/null

--- template/config_run.bash
+++ template/config_run.bash
@@ -15,12 +15,19 @@ fi
 
 mce_cmd -qf $mce_script
 
+
+if [ "$tune_id" == "" ]; then
+    # No tuning, so no runfile block
+    SOURCE_TUNE=/dev/null
+else
+    SOURCE_TUNE=/data/cryo/${tune_id}
+fi
+
 # Change the last_squid_tune link
 LAST_TUNE=/data/cryo/last_squid_tune
-SOURCE_TUNE=/data/cryo/${tune_id}
 
 # ... but only if everything seems reasonable.
-if [ -e "$SOURCE_TUNE" ] && ![ -d "$SOURCE_TUNE" ] && ( \
+if [ -e "$SOURCE_TUNE" ] && ! [ -d "$SOURCE_TUNE" ] && ( \
     ( [ -h "$LAST_TUNE" ] && rm "$LAST_TUNE" ) || \
     ! [ -e "$LAST_TUNE" ] ); then
     ln -s "$SOURCE_TUNE" "$LAST_TUNE"
