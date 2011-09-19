4c56ccc33c15d5ba0d4ac1d773d918f7c07a6337
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2011-09-19 03:33:06 +0000

    Fix last commit.

--- python/auto_setup/config/__init__.py
+++ python/auto_setup/config/__init__.py
@@ -6,5 +6,5 @@ from get_exp_config import get_exp_param
 from get_exp_config import set_exp_param
 from get_exp_config import set_exp_param_range
 from get_exp_config import mas_param
-from get_exp_config import exptFile
+from get_exp_config import configFile
 from get_exp_config import get_fake_expt  #transitional
