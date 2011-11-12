9c5489ccdc2370c2c8656ef1119974357b2e29c2
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2011-11-12 00:18:52 +0000

    Improvements to sa_offset_bias_ratio estimation.

--- python/special_ops.py
+++ python/special_ops.py
@@ -24,7 +24,8 @@ for action in args:
         print 'Storing SA bias...'
         sa_bias0 = m.read('sa', 'bias')
         sa_offset0 = m.read('sa', 'offset')
-        
+        sample_num = m.read('rca', 'sample_num')[0]
+
         print 'Measuring SA bias response...'
         n_sa = len(sa_bias0)
         step = 1000
@@ -48,10 +49,27 @@ for action in args:
         if any(d_offset==0):
             ratio[d_offset==0] = 0
         
-        print 'Apparent sa_offset_bias_ratios, by column: '
+        print 'Apparent resistance ratios, by column:'
         for r in range((n_sa+7)/8):
             print '  ', ' '.join(['%6.3f' % x for x in ratio[r*8:(r+1)*8]])
-        print 'So you probably want to use:  %.3f' % ratio[ratio!=0].mean()
-        
+        ratio_mean = ratio[ratio!=0].mean()
+        print 'The typical ratio is:  %.3f' % ratio_mean
+
+        # This ratio isn't necessarily what you want to use, you might
+        # want to let the floor drop out gradually
+        sa_max = 64000. / step
+        sa_fall = -8000*sample_num - y0
+        ratio1 = -(sa_fall / sa_max - d_bias) / d_offset
+        ratio1[d_offset==0] = 0.
+        print 'Suggested sa_offset_bias_ratios, by column:'
+        for r in range((n_sa+7)/8):
+            print '  ', ' '.join(['%6.3f' % x for x in ratio1[r*8:(r+1)*8]])
+        ratio1_min = ratio1[ratio1!=0].min()
+        print 'Smallest range-filling ratio: %.3f' % ratio1_min 
+        # Rough badness diagnostic
+        if ratio1_min < ratio_mean:
+            print 'You have weird ratios.  Seek advice.'
+        print 'Recommended sa_offset_bias_ratio: %.3f' % ratio1_min
+
     else:
         o.error("I do not know how to '%s'" % action)
