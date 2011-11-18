f21d72815b3f89d4646b780bcbd29744725da578
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2011-11-18 00:12:54 +0000

    Verbosify rect_check, and add a check for the duplicate data bug.

--- python/rect_check.py
+++ python/rect_check.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+USAGE="""%prog [options] [runfiles]
+
+Query the MCE, or parse a runfile, to evaluate the sanity of the
+rectangle mode configuration.
+
+We take this opportunity to remind the user, again, that all readout
+modes are rectangle readout modes.
+"""
+
 from numpy import *
 
 MCE_CLOCK = 5e7 # Hz
@@ -19,6 +28,9 @@ class frameConfig:
         ('rc_nc',   ('rca', 'num_cols_reported')),
         ('rc_r0',   ('rca', 'readout_row_index')),
         ('rc_c0',   ('rca', 'readout_col_index')),
+        # Params below this point are not summarized
+        ('barrier', (None, None)),
+        ('rc_fw',   ('rca', 'fw_rev')),
         ]
 
     def __init__(self, mce=None):
@@ -57,34 +69,50 @@ class frameConfig:
             self.params['cc_nmux']
         #
         d['f_sam'] = d['f_ro'] * (d['n_ro'] / d['n_mux'])
+        #
+        d['dup_bug'] = (self.params['rc_fw'] < 0x5010007) and (
+            self.params['rc_nmux']*self.params['rc_cmux'] < 230 + d['n_ro']*2)
         self.derived = d
 
     def report(self):
         print 'MCE configuration:'
         for k, (c,p) in self.mce_params:
+            if k == 'barrier':
+                break
             print ' %-8s %-30s %3i' % \
                 (k, '( %s, %s ):'%(c,p), self.params[k])
+        print
         print 'Framing:'
         d = self.derived
         def yn(x):
             if x: return 'yes'
             return 'no'
-        print ' RC storage per mux cycle:              %4i' % d['n_mux']
-        print ' CC read-out per frame:                 %4i' % d['n_ro']
+        print ' RC words stored per mux cycle:         %4i' % d['n_mux']
+        print ' CC words per read-out frame:           %4i' % d['n_ro']
         print ' CC decimation:                         %4i' % self.params['cc_dec']
-        print ' Contiguous?                            %4s' % yn(d['contiguous'])
-        print ' Complete?                              %4s' % yn(d['complete'])
-        print ' Bizarro?                               %4s' % yn(d['bizarro'])
-        print ' Bounded?                               %4s' % yn(d['in_bounds'])
+        print
+        print 'Sanity summary:'
+        print ' Contiguous (for high-rate readout)?    %4s' % yn(d['contiguous'])
+        print ' Complete   (all-detector readout)?     %4s' % yn(d['complete'])
+        print ' Bizarro    (a bad thing)?              %4s' % yn(d['bizarro'])
+        print ' Bounded    (a good thing)?             %4s' % yn(d['in_bounds'])
+        if d['dup_bug']:
+            print ' Duplicate data bug!                    %4s' % yn(d['dup_bug'])
+        print
         print 'Timing:'
         print ' Mux freq:                         %9.2f' % d['f_mux']
         print ' Mean sampling freq:               %9.2f' % d['f_sam']
         print ' Read-out freq:                    %9.2f' % d['f_ro']
+        print
         print 'Data volume:'
         print ' Frame size (bytes/RC):            %9i' % \
             (4*(MCE_OVERHEAD + d['n_ro']))
         print ' Data rate (MB/s/RC):              %9.2f' % \
             (1.e-6*d['f_ro'] * 4*(MCE_OVERHEAD + d['n_ro']))
+        if d['dup_bug']:
+            print
+            print 'Warning!! your RC firmware has the duplicate data bug;'
+            print '          and this configuration will suffer from it.'
  
     def from_runfile(self, filename):
         rf = MCERunfile(filename)
@@ -99,7 +127,10 @@ class frameConfig:
         if mce == None:
             mce = self.mce
         for k, p in self.mce_params:
-            self.params[k] = mce.read(p[0], p[1])[0]
+            if k == 'barrier':
+                self.params[k] = 1
+            else:
+                self.params[k] = mce.read(p[0], p[1])[0]
         self.derive()
 
 
@@ -107,7 +138,8 @@ if __name__ == '__main__':
     import sys
     from optparse import OptionParser
     o = OptionParser()
-    o.add_option('-m', '--mce',action='store_true',default=False)
+    o.add_option('-m', '--mce',action='store_true',default=False,
+                 help='Query MCE rather than parsing a runfile')
     opts, args = o.parse_args()
 
     if opts.mce: 
