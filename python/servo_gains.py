c314a7314af42344dcc3c6a88e36047881e6cb1f
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2012-01-23 03:37:16 +0000

    Improvements to servo_gains.py

--- python/servo_gains.py
+++ python/servo_gains.py
@@ -9,10 +9,6 @@ from pylab import *
 
 from optparse import OptionParser
 
-o = OptionParser(usage="""
-               %prog [options] tuning_dir
-
-where tuning_dir is the folder containg the tuning data.""")
 
 def pretty(x, n=8, fmt='%8.4f'):
     out = ''
@@ -20,23 +16,36 @@ def pretty(x, n=8, fmt='%8.4f'):
         out += ' '.join([fmt % _x for _x in x[i*n:i*n+n]]) + '\n'
     return out
 
+
+o = OptionParser(usage="""
+               %prog [options] tuning_dir
+
+where tuning_dir is the folder containg the tuning data.""")
+o.add_option('-s','--stage',action='append')
+
 opts, args = o.parse_args()
+
+if opts.stage == None:
+    opts.stage = ['sa_ramp', 'sq2_servo']
 
 for target in args:
     fs = aset.util.FileSet(target)
-    sa = aset.SARamp.join([aset.SARamp(f) for f in fs.stage_all('sa_ramp')])
-    sq2 = aset.SQ2Servo.join([aset.SQ2Servo(f) for f in fs.stage_all('sq2_servo')])
+    if 'sa_ramp' in opts.stage:
+        sa = aset.SARamp.join([aset.SARamp(f) for f in fs.stage_all('sa_ramp')])
+        sa.reduce(slope=1)
+        s2s_gains = 1./sa.analysis['lock_slope']
+        print 'SQ2 servo critical gain: '
+        print pretty(s2s_gains)
 
-    print target
-    sa.reduce(slope=1)
-    s2s_gains = 1./sa.analysis['lock_slope']
-
-    print 'SQ2 servo critical gain: '
-    print pretty(s2s_gains)
-
+    if 'sq2_servo' in opts.stage:
+        sq2 = aset.SQ2Servo.join([aset.SQ2Servo(f) \
+                                      for f in fs.stage_all('sq2_servo')])
     for s in [-1, +1]:
         sq2.reduce(slope=s)
         s1s_gains = 1./sq2.analysis['lock_slope'] / s2s_gains
         print 'SQ1 servo critical gain (sq2_slope=%i):' % s
         print pretty(s1s_gains)
         print
+
+    print target
+
