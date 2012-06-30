5cafa996e89b00e5074b724e3da17b9ae12a81dd
mhasse <mhasse@35c7d0d9-0540-0410-a198-bddc676e2512>
2012-06-30 20:19:55 +0000

    plotters.py, with plot control class.

--- utilities/headsup/plotters.py
+++ utilities/headsup/plotters.py
@@ -1,9 +1,13 @@
 import clients
+import numpy as np
 
 display_defaults = {
     'autoscale': True,
-    'zrange': (-1000,1000),
+    'zrange': [-1000,1000],
     }
+
+def mask_to_list(ar):
+    return [[int(x) for x in y] for y in ar]
 
 class displayController(dict, clients.dataProducer):
     def __init__(self, addr, name='dispctrl', field=None):
@@ -30,12 +34,41 @@ class displayController(dict, clients.dataProducer):
     def zrange(self, min=None, max=None):
         post = False
         if min != None:
-            self['zrange'] = min
+            self['zrange'][0] = min
             post = True
         if max != None:
-            self['zrange'] = max
+            self['zrange'][1] = max
             post = True
         if post:
             self.post_some(['zrange'])
         return self['zrange']
+
+    def set_mask(self, mask=None):
+        if mask == None:
+            return self['mask']
+        # json doesn't like numpy's ints.
+        self['mask'] = [[int(x) for x in y] for y in mask]
+        self.post_some(['mask'])
+
+    def mask_area(self, row=None, col=None, shape=None, unmask=False):
+        if 'mask' not in self:
+            if shape != None:
+                mask = np.zeros(shape, 'bool')
+            else:
+                print 'Set mask shape first.'
+                return False
+        else:
+            mask = np.asarray(self['mask'], 'bool').transpose()
+        if row != None and col != None:
+            mask[row, col] = False ^ unmask
+        elif row != None:
+            mask[row,:] = False ^ unmask
+        elif col != None:
+            mask[:,col] = False ^ unmask
+        else:
+            mask[:,:] = False ^ unmask
+        self.set_mask(mask_to_list(mask.transpose()))
+        
+    def unmask_area(self, row=None, col=None, shape=None):
+        return self.mask_area(row, col, shape, unmask=True)
 
